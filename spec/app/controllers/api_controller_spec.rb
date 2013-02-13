4570c0c3386181ddf60b4e1ca6138ebdb1f6d23c
Lloyd <lloydmeta@gmail.com>
2013-02-13 18:08:01 +0900

    Add test case for mobile credentials

--- spec/app/controllers/api_controller_spec.rb
+++ spec/app/controllers/api_controller_spec.rb
@@ -7,12 +7,25 @@ describe "ApiController", :type => :controller do
   end
 
   describe "get /" do
+
     context "with server credentials" do
+
       it "should return the current service" do
         get '/', params={}, rack_env=credentials_to_headers(server_api_auth_params(@service.server_client_id, @service.server_client_secret))
         JSON.parse(last_response.body)["server_client_secret"].should eq(@service.server_client_secret)
       end
+
+    end
+
+    context "with mobile credentials" do
+
+      it "should return the current service" do
+        get '/', params={}, rack_env=credentials_to_headers(mobile_api_auth_params(@service.mobile_client_id, @service.mobile_client_secret))
+        JSON.parse(last_response.body)["mobile_client_secret"].should eq(@service.mobile_client_secret)
+      end
+
     end
 
   end
+
 end
