c1e6546a127ff6cc7e4b469d6c4311a571c244eb
Lloyd <lloydmeta@gmail.com>
2013-02-13 19:22:05 +0900

    More tests

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -13,20 +13,22 @@ PushToDeviceServer.controllers :users do
   post :create, :map => "/users/", :provides => :json do
     content_type :json
 
+    error 433, {error: "unique_hash not provided"}.to_json unless params["unique_hash"]
+
     @service_user = api_current_user.users.where(
       unique_hash: params["unique_hash"]
     ).first_or_create!
 
-    if params[:apn_device_token]
-      @service_user.apn_device_tokens.where(
-        apn_device_token: params["apn_device_token"]
-      ).first_or_create!
+    if params["apn_device_token"]
+      if @service_user.apn_device_tokens.where(apn_device_token: params["apn_device_token"]).empty?
+        @service_user.apn_device_tokens.build(apn_device_token: params["apn_device_token"]).save!
+      end
     end
 
-    if params[:gcm_registration_id]
-      @service_user.gcn_device_tokens.where(
-        gcm_registration_id: params["gcm_registration_id"]
-      ).first_or_create!
+    if params["gcm_registration_id"]
+      if @service_user.gcm_device_tokens.where(gcm_registration_id: params["gcm_registration_id"]).empty?
+        @service_user.gcm_device_tokens.build(gcm_registration_id: params["gcm_registration_id"]).save!
+      end
     end
 
     @service_user.to_json
--- spec/app/controllers/users_controller_spec.rb
+++ spec/app/controllers/users_controller_spec.rb
@@ -10,6 +10,11 @@ describe "UsersController", :type => :controller do
 
     context "with mobile credentials" do
 
+      it "should fail if unique_hash not present" do
+        post '/users', params={}, rack_env=credentials_to_headers(mobile_api_auth_params(@service.mobile_client_id, @service.mobile_client_secret))
+        last_response.should_not be_successful
+      end
+
       context "only with unique_hash" do
 
         it "should create a user" do
@@ -36,6 +41,135 @@ describe "UsersController", :type => :controller do
             post '/users', params={"unique_hash" => "hahahlalala1234"}, rack_env=credentials_to_headers(mobile_api_auth_params(@service.mobile_client_id, @service.mobile_client_secret))
             post '/users', params={"unique_hash" => "hahahlalala1234"}, rack_env=credentials_to_headers(mobile_api_auth_params(another_service.mobile_client_id, another_service.mobile_client_secret))
           }.to change(User, :count).by(2)
+        end
+
+      end
+
+    end # end only unique_hash
+
+    context "with unique_hash and apn_device_token" do
+
+      context "new user, new apn_device_token" do
+
+        it "should create a new user" do
+          expect{
+            post '/users', params={"unique_hash" => "hahahlalala1234", "apn_device_token" => "asdf1234"}, rack_env=credentials_to_headers(mobile_api_auth_params(@service.mobile_client_id, @service.mobile_client_secret))
+          }.to change(User, :count).by(1)
+        end
+
+        it "should create a new apn_device_token on the user" do
+          post '/users', params={"unique_hash" => "hahahlalala1234", "apn_device_token" => "asdf1234"}, rack_env=credentials_to_headers(mobile_api_auth_params(@service.mobile_client_id, @service.mobile_client_secret))
+          User.find_by(unique_hash: "hahahlalala1234").apn_device_tokens.count.should eq(1)
+        end
+
+        it "should create a new apn_device_token_on this user that contains the same apn_device_token" do
+          post '/users', params={"unique_hash" => "hahahlalala1234", "apn_device_token" => "asdf1234"}, rack_env=credentials_to_headers(mobile_api_auth_params(@service.mobile_client_id, @service.mobile_client_secret))
+          User.find_by(unique_hash: "hahahlalala1234").apn_device_tokens.first.device_id.should eq("asdf1234")
+        end
+
+      end
+
+      context "old user, new apn_device_token" do
+
+        before(:each) do
+          user = User.create(unique_hash: "hahahlalala1234", service: @service)
+          user.apn_device_tokens.build(apn_device_token: "xyz1234").save!
+        end
+
+        it "should not create a new user" do
+          expect{
+            post '/users', params={"unique_hash" => "hahahlalala1234", "apn_device_token" => "asdf1234"}, rack_env=credentials_to_headers(mobile_api_auth_params(@service.mobile_client_id, @service.mobile_client_secret))
+          }.to_not change(User, :count)
+        end
+
+        it "should create a new apn_device_token on the user" do
+          post '/users', params={"unique_hash" => "hahahlalala1234", "apn_device_token" => "asdf1234"}, rack_env=credentials_to_headers(mobile_api_auth_params(@service.mobile_client_id, @service.mobile_client_secret))
+          User.find_by(unique_hash: "hahahlalala1234").apn_device_tokens.count.should eq(2)
+        end
+
+      end
+
+      context "old user, old apn_device_token" do
+
+        before(:each) do
+          user = User.create(unique_hash: "hahahlalala1234", service: @service)
+          user.apn_device_tokens.build(apn_device_token: "asdf1234").save!
+        end
+
+        it "should not create a new user" do
+          expect{
+            post '/users', params={"unique_hash" => "hahahlalala1234", "apn_device_token" => "asdf1234"}, rack_env=credentials_to_headers(mobile_api_auth_params(@service.mobile_client_id, @service.mobile_client_secret))
+          }.to_not change(User, :count)
+        end
+
+        it "should not create a new apn_device_token on the user" do
+          post '/users', params={"unique_hash" => "hahahlalala1234", "apn_device_token" => "asdf1234"}, rack_env=credentials_to_headers(mobile_api_auth_params(@service.mobile_client_id, @service.mobile_client_secret))
+          User.find_by(unique_hash: "hahahlalala1234").apn_device_tokens.count.should eq(1)
+        end
+
+      end
+
+    end
+
+    context "with unique_hash and gcm_registration_id" do
+
+      context "new user, new gcm_registration_id" do
+
+        it "should create a new user" do
+          expect{
+            post '/users', params={"unique_hash" => "hahahlalala1234", "gcm_registration_id" => "asdf1234"}, rack_env=credentials_to_headers(mobile_api_auth_params(@service.mobile_client_id, @service.mobile_client_secret))
+          }.to change(User, :count).by(1)
+        end
+
+        it "should create a new gcm_device_token on the user" do
+          post '/users', params={"unique_hash" => "hahahlalala1234", "gcm_registration_id" => "asdf1234"}, rack_env=credentials_to_headers(mobile_api_auth_params(@service.mobile_client_id, @service.mobile_client_secret))
+          User.find_by(unique_hash: "hahahlalala1234").gcm_device_tokens.count.should eq(1)
+        end
+
+
+        it "should create a new gcm_device_token this user that contains the same apn_device_token" do
+          post '/users', params={"unique_hash" => "hahahlalala1234", "gcm_registration_id" => "asdf1234"}, rack_env=credentials_to_headers(mobile_api_auth_params(@service.mobile_client_id, @service.mobile_client_secret))
+          User.find_by(unique_hash: "hahahlalala1234").gcm_device_tokens.first.device_id.should eq("asdf1234")
+        end
+
+      end
+
+      context "old user, new gcm_registration_id" do
+
+        before(:each) do
+          user = User.create(unique_hash: "hahahlalala1234", service: @service)
+          user.gcm_device_tokens.build(gcm_registration_id: "xyz1234").save!
+        end
+
+        it "should not create a new user" do
+          expect{
+            post '/users', params={"unique_hash" => "hahahlalala1234", "gcm_registration_id" => "asdf1234"}, rack_env=credentials_to_headers(mobile_api_auth_params(@service.mobile_client_id, @service.mobile_client_secret))
+          }.to_not change(User, :count)
+        end
+
+        it "should create a new gcm_device_token on the user" do
+          post '/users', params={"unique_hash" => "hahahlalala1234", "gcm_registration_id" => "asdf1234"}, rack_env=credentials_to_headers(mobile_api_auth_params(@service.mobile_client_id, @service.mobile_client_secret))
+          User.find_by(unique_hash: "hahahlalala1234").gcm_device_tokens.count.should eq(2)
+        end
+
+      end
+
+      context "old user, old gcm_registration_id" do
+
+        before(:each) do
+          user = User.create(unique_hash: "hahahlalala1234", service: @service)
+          user.gcm_device_tokens.build(gcm_registration_id: "asdf1234").save!
+        end
+
+        it "should not create a new user" do
+          expect{
+            post '/users', params={"unique_hash" => "hahahlalala1234", "gcm_registration_id" => "asdf1234"}, rack_env=credentials_to_headers(mobile_api_auth_params(@service.mobile_client_id, @service.mobile_client_secret))
+          }.to_not change(User, :count)
+        end
+
+        it "should not create a new gcm_device_token on the user" do
+          post '/users', params={"unique_hash" => "hahahlalala1234", "gcm_registration_id" => "asdf1234"}, rack_env=credentials_to_headers(mobile_api_auth_params(@service.mobile_client_id, @service.mobile_client_secret))
+          User.find_by(unique_hash: "hahahlalala1234").gcm_device_tokens.count.should eq(1)
         end
 
       end
