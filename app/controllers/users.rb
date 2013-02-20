032b5bf80f8147523f60aa1bfc975142cb33d5bd
Lloyd <lloydmeta@gmail.com>
2013-02-20 20:34:20 +0900

    Cors again

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -9,7 +9,7 @@ PushToDeviceServer.controllers :users do
   end
 
   # preflight CORS
-  options '/users/' do
+  options :cors_preflight, :map => '/users/' do
     cors_headers
     " "
   end
@@ -19,36 +19,31 @@ PushToDeviceServer.controllers :users do
   # as well as optionally apn_device_token and/or gcm_registration_id
   post :create, :map => "/users/", :provides => :json do
     content_type :json
-    if request.env["REQUEST_METHOD"] == "OPTIONS"
-      cors_headers
-      " "
-    else
-      begin
-        data=JSON.parse(request.body.read.to_s)
-      rescue
-        error 422, {error: "invalid json"}.to_json
-      end
-
-      error 422, {error: "unique_hash not provided"}.to_json unless data["unique_hash"]
-
-      @service_user = api_current_user.users.where(
-        unique_hash: data["unique_hash"]
-      ).first_or_create!
-
-      if data["apn_device_token"]
-        if @service_user.apn_device_tokens.where(apn_device_token: data["apn_device_token"]).empty?
-          @service_user.apn_device_tokens.build(apn_device_token: data["apn_device_token"]).save!
-        end
-      end
-
-      if data["gcm_registration_id"]
-        if @service_user.gcm_device_tokens.where(gcm_registration_id: data["gcm_registration_id"]).empty?
-          @service_user.gcm_device_tokens.build(gcm_registration_id: data["gcm_registration_id"]).save!
-        end
-      end
-
-      @service_user.to_json
+    begin
+      data=JSON.parse(request.body.read.to_s)
+    rescue
+      error 422, {error: "invalid json"}.to_json
     end
+
+    error 422, {error: "unique_hash not provided"}.to_json unless data["unique_hash"]
+
+    @service_user = api_current_user.users.where(
+      unique_hash: data["unique_hash"]
+    ).first_or_create!
+
+    if data["apn_device_token"]
+      if @service_user.apn_device_tokens.where(apn_device_token: data["apn_device_token"]).empty?
+        @service_user.apn_device_tokens.build(apn_device_token: data["apn_device_token"]).save!
+      end
+    end
+
+    if data["gcm_registration_id"]
+      if @service_user.gcm_device_tokens.where(gcm_registration_id: data["gcm_registration_id"]).empty?
+        @service_user.gcm_device_tokens.build(gcm_registration_id: data["gcm_registration_id"]).save!
+      end
+    end
+
+    @service_user.to_json
   end
 
   # for receiving POST requests to /users/{unique_hash}/notifications
