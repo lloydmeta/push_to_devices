3986fe95317ddeacbe2369e3020d9413536db774
Lloyd <lloydmeta@gmail.com>
2013-02-20 20:06:29 +0900

    Allow all origins

--- lib/cors_headers.rb
+++ lib/cors_headers.rb
@@ -2,7 +2,7 @@
 module CorsHelpers
   private
   def cors_headers
-    response.headers['Access-Control-Allow-Origin'] = request.env['HTTP_ORIGIN']
+    response.headers['Access-Control-Allow-Origin'] = "*"
     response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
     response.headers['Access-Control-Allow-Headers'] = %w{accept timestamp origin x-csrf-token server-client-id mobile-client-id client-sig Origin Accept Content-Type X-Requested-With X-CSRF-Token}.join(","),
     response.headers['Access-Control-Max-Age'] = (60 * 60 * 6).to_s
