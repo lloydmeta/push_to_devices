da243ff89ed8b8e9bf6a5f0e616e3aa5b1734c45
Lloyd <lloydmeta@gmail.com>
2013-03-13 12:15:02 +0900

    Add feedback_fail_count field and increment and decrement methods for this field in APNDeviceToken class

--- models/apn_device_token.rb
+++ models/apn_device_token.rb
@@ -4,6 +4,7 @@ class ApnDeviceToken < DeviceToken
 
   # field <name>, :type => <type>, :default => <value>
   field :apn_device_token, :type => String
+  field :feedback_fail_count, :type => Integer, :default => 0
 
   embedded_in :user
 
@@ -15,6 +16,14 @@ class ApnDeviceToken < DeviceToken
 
   def device_id
     apn_device_token
+  end
+
+  def increment_feedback_fail_count(increment_by = 1)
+    inc(:feedback_fail_count, increment_by)
+  end
+
+  def decrement_feedback_fail_count(decrement_by = -1)
+    increment_feedback_fail_count(decrement_by)
   end
 
 end
